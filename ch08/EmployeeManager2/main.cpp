eb63e4197feae0f2cd1f5236522c175331c6bf0f
mango0422 <syj0422@gachon.ac.kr>
2022-02-07 15:39:23 +0900

    new file

--- ch08/EmployeeManager2/main.cpp
+++ ch08/EmployeeManager2/main.cpp
@@ -1,18 +1,14 @@
 #include "CLASS.h"
 
 int main(void){
-    // ���������� �������� ����� ��Ʈ�� Ŭ������ ��ü����
     EmployeeHandler handler;
 
-    // ���� ���
     handler.AddEmployee(new PermanentWorker("KIM", 1000));
     handler.AddEmployee(new PermanentWorker("LEE", 1500));
     handler.AddEmployee(new PermanentWorker("JUN", 2000));
 
-    // �̹� �޿� �����ؾ� �� �޿��� ����
     handler.ShowAllSalaryInfo();
 
-    // �̹� �޿� �����ؾ� �� �޿��� ����
     handler.ShowTotalSalary();
     return (0);
 

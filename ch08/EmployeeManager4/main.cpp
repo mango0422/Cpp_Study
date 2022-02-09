e512c6fc3c4b5bbc88f423fe52dbd52833057978
mango0422 <syj0422@gachon.ac.kr>
2022-02-09 18:26:35 +0900

    modified file

--- ch08/EmployeeManager4/main.cpp
+++ ch08/EmployeeManager4/main.cpp
@@ -3,5 +3,7 @@
 int main(void){
     SalesWorker seller("Hong", 1000, 0.1);
     cout<<seller.GetPay()<<endl;
+    // cout<<seller.PermanentWorker::Getpay()<<endl;
     seller.ShowSalaryInfo();
+    // seller.PermanentWorker::ShowSalaryInfo();
 }

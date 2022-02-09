db69f1aad7b02601d9d98862b676f4a02f26660b
mango0422 <syj0422@gachon.ac.kr>
2022-02-09 18:24:44 +0900

    modified file

--- ch08/EmployeeManager4/main.cpp
+++ ch08/EmployeeManager4/main.cpp
@@ -1,15 +1,7 @@
 #include "CLASS.h"
 
 int main(void){
-    EmployeeHandler handler;
-
-    handler.AddEmployee(new PermanentWorker("KIM", 1000));
-    handler.AddEmployee(new PermanentWorker("LEE", 1500));
-    handler.AddEmployee(new PermanentWorker("JUN", 2000));
-
-    handler.ShowAllSalaryInfo();
-
-    handler.ShowTotalSalary();
-    return (0);
-
+    SalesWorker seller("Hong", 1000, 0.1);
+    cout<<seller.GetPay()<<endl;
+    seller.ShowSalaryInfo();
 }

6442bffa3f906f97335e514e4d09ff6ab2cbdce8
mango0422 <syj0422@gachon.ac.kr>
2022-02-17 15:13:09 +0900

    new file 1, modified file 1

--- ch08/VirtualDestructor.cpp
+++ ch08/VirtualDestructor.cpp
@@ -9,6 +9,29 @@ class First{
         char * strOne;
     public:
         First(char * str){
-            strOne = new char[strlen(str) + 1];
+            strOne = new char[strlen(str) + 1];  // First 클래스의 생성자에서 동적 할당이 있었기 때문에 소멸자가 적절히 정의되었다.
+        }
+        ~First(){
+            cout<<"~First()"<<endl;
+            delete []strOne;  // First 클래스의 생성자에서 동적 할당이 있었기 때문에 소멸자가 적절히 정의되었다.
         }
 };
+
+class Second : public First{
+    private:
+        char * strTwo;
+    public:
+        Second(char * str1, char * str2) : First(str1){
+            strTwo = new char[strlen(str2) + 1];
+        }
+        ~Second(){
+            cout<<"~Second()"<<endl;
+            delete []strTwo;
+        }
+};
+
+int main(void){
+    First * ptr=new Second("simple", "complex");
+    delete ptr;
+    return (0);
+}

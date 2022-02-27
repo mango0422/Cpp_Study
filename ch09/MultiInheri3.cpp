fe931102e50275be035d57905c35e412f6610553
mango0422 <syj0422@gachon.ac.kr>
2022-02-27 18:01:19 +0900

    modified file 1

--- ch09/MultiInheri3.cpp
+++ ch09/MultiInheri3.cpp
@@ -36,7 +36,7 @@ class MiddleDerivedTwo : virtual public Base{
         }
 };
 
-class LastDerived : public MiddleDerivedOne, public MiddleDerivedTwo{
+class LastDerived : public MiddleDerivedOne, public MiddleDerivedTwo{ // Base 클래스를 간접적으로 두번 상속함.
     public:
         LastDerived() : MiddleDerivedOne(), MiddleDerivedTwo(){
             cout<<"LastDerived Constructor"<<endl;
@@ -55,3 +55,16 @@ int main(void){
     ldr.ComplexFunc();
     return (0);
 }
+
+/*
+ * MiddleDerivedOne 클래스와 MiddleDerivedTwo 클래스가 virtual 선언이 되지 않은 채로 Base 클래스를 상속하고 객체가 생성된다면,
+ * 어느 클래스를 통해 간접 상속한 Base 클래스의 멤버함수를 호출할 것인지를 명시해야 한다.
+ * 
+ * MiddleDerivedOne::SimpleFunc(); // MiddleDerivedOne 클래스가 상속한 Base 클래스의 SimpleFunc 함수호출을 명령!
+ * MiddleDerivedTwo::SimpleFunc(); // MiddleDerivedTwo 클래스가 상속한 Base 클래스의 SimpleFunc 함수호출을 명령!
+ * 
+ * 그런데 이러한 상황에서, Base 클래스의 멤버가 LastDerived 객체에 하나씩만 존재하는 것이 타당한 경우가 대부분이다.
+ * 즉, Base 클래스를 딱 한번만 상속하게끔 하는 것이 더 현실적인 해결책이 될 수 있다.
+ * 그리고 이를 위한 문법이 바로 '가상 상속'이다.
+ * 가상으로 상속을 하는 방법은 위 예제에서 보이고 있다.
+ */
